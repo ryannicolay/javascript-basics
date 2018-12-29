61e6cbf3785110dba5a20d69a18badd81f6c8464
Ryan Nicolay <ryannicolay@gmail.com>
2018-12-29 08:16:25 -0500

    Finished Objects exercises

--- objects.js
+++ objects.js
@@ -15,9 +15,9 @@ showAddress(address);
 
 // Exercise 2 - Factory and Constructor Functions
 
-let address2 = createAddress("123 Main St", "Jacksonville", "32065");
+let address1 = createAddress("123 Main St", "Jacksonville", "32065");
 
-console.log(address2);
+console.log(address1);
 
 function createAddress(street, city, zipCode) {
     return {
@@ -27,7 +27,7 @@ function createAddress(street, city, zipCode) {
     };
 }
 
-address2 = Address("123 Main St", "Jacksonville", "32065")
+let address2 = new Address("123 Main St", "Jacksonville", "32065");
 
 console.log(address2);
 
@@ -38,3 +38,55 @@ function Address(street, city, zipCode) {
 }
 
 // Exercise 3 - Object Equality
+
+function areEqual(address1, address2) {
+    return address1.street === address2.street && address1.city === address2.city && address1.zipCode === address2.zipCode;
+}
+
+function areSame(address1, address2) {
+    return address1 === address2;
+}      
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+
+// Exercise 4 - Blog Post Object
+
+let blogPost = {
+    title: "a",
+    body: "b",
+    author: "c",
+    views: 1,
+    comments: [
+        { author: "d", body: "e"},
+        { author: "f", body: "g"}
+    ],
+    isLive: true
+};
+
+// Exercise 5 - Constructor Functions
+
+function BlogPost(title, body, author) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = fasle;
+}
+
+// Exercise 6 - Price Range Object
+    
+let priceRange = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+    { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
+];
+
+    // Restaurant Object
+        // Store Name
+        // Address
+        // Rating
+        // Categories
+        // Description
+        // Average Price Per Person
